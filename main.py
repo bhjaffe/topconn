928610f976956791565b4d5bee78b760a26c8c6c
Brian Jaffe <bhjaffe@gmail.com>
2015-05-03 22:14:15 -0700

    fixed overshoot issues; added comments

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ from ship import Ship
 
 
 def ship_info(ship):
-	# print '{:20s} {:20s} {:20s}'.format("COURSE", "SPEED", "RUDDER")
 	print '{:15s} {:15s} {:15s}'.format(str(ship.course), str(ship.speed), str(ship.rudder_angle))
-	# print 'Ship course: ' + str(ship.course)
-	# print 'Ship speed:  ' + str(ship.speed)
-	# print 'Ship rudder: ' + str(ship.rudder_angle)
 
 def new_course_speed_rudder(ship, course, speed, rudder):
 	ship.set_new_course(course)
@@ -33,4 +29,7 @@ new_course_speed_rudder(myShip, 180, 20, 10)
 for i in range(60):
 	one_second(myShip)
 	ship_info(myShip)
-
+new_course_speed_rudder(myShip, 15, 10, -15)
+for i in range(60):
+	one_second(myShip)
+	ship_info(myShip)
--- ship.py
+++ ship.py
@@ -8,17 +8,25 @@ class Ship(object):
 		self.speed_ordered = speed
 		self.rudder_angle = 0.0
 		self.rudder_angle_ordered = 0.0
-		self.rate_speed_change = 0.2 # knots / second
-		self.rate_rudder_change = 2.0 # degrees / second
-		self.rate_course_change = 0.02 # degrees / (knots_of_speed * degrees_of_rudder * second)
-		self.time_rudder_ease = 5.0 # seconds looking ahead to reduce turn rate to not overshoot desired course
-		self.rudder_angle_reduction_factor = 1.5 # factor by which rudder angle is reduced when overshoot is approaching
+		self.rate_speed_change = 0.2        # knots / second
+		self.rate_rudder_change = 2.0       # degrees / second
+		self.rate_course_change = 0.02      # degrees / (knots_of_speed * degrees_of_rudder * second)
+		self.time_rudder_ease = 5.0         # seconds looking ahead to reduce turn rate to not overshoot desired course
+		self.rudder_angle_reduction_factor = 1.5        # factor by which rudder angle is reduced when overshoot is approaching
 
 	@staticmethod
-	def correct_360_math(number_of_degrees): # corrects for 359 + 1 degrees = 0 degrees, etc.
+	def correct_360_math(number_of_degrees):        # corrects for 359 + 1 degrees = 0 degrees, etc.
 		if number_of_degrees >= 360:
 			return number_of_degrees - 360
 		if number_of_degrees < 0:
+			return number_of_degrees+ 360
+		return number_of_degrees
+
+	@staticmethod
+	def correct_180_math(number_of_degrees):        # corrects for turns to be +/- 180 or less
+		if number_of_degrees > 180:
+			return number_of_degrees - 360
+		if number_of_degrees <= -180:
 			return number_of_degrees+ 360
 		return number_of_degrees
 
@@ -31,19 +39,21 @@ class Ship(object):
 	def set_new_speed(self, new_speed):
 		self.speed_ordered = new_speed
 
-	def change_rudder_angle(self):
+	def change_rudder_angle(self):      # increases and decreases rudder angle to enter and exit turns
 		if self.rudder_angle != 0:
-			seconds_to_overshoot = self.correct_360_math(self.course_ordered - self.course) / (self.speed*self.rudder_angle*self.rate_course_change) # number of seconds at current turn rate until ship overshoots turn
+			seconds_to_overshoot = self.correct_180_math(self.course_ordered - self.course) / (self.speed*self.rudder_angle*self.rate_course_change)        # number of seconds at current turn rate until ship overshoots turn
 		else:
-			seconds_to_overshoot = 999
+			seconds_to_overshoot = 999      # dummy entry for very large number, avoids divide by zero when rudder angle = 0
 		if seconds_to_overshoot < self.time_rudder_ease:
 			self.rudder_angle = self.rudder_angle / self.rudder_angle_reduction_factor
 			self.rudder_angle_ordered = self.rudder_angle
-		elif self.rudder_angle != self.rudder_angle_ordered: # increase abs(rudder_angle) to increase turn rate
+		elif self.rudder_angle != self.rudder_angle_ordered and abs(self.rudder_angle - self.rudder_angle_ordered) >= self.rate_rudder_change: # increase abs(rudder_angle) to increase turn rate
 			if self.rudder_angle_ordered > self.rudder_angle:
 				self.rudder_angle += self.rate_rudder_change
 			else:
 				self.rudder_angle -= self.rate_rudder_change
+		elif self.rudder_angle != self.rudder_angle_ordered and abs(self.rudder_angle - self.rudder_angle_ordered) < self.rate_rudder_change:
+			self.rudder_angle = self.rudder_angle_ordered # avoids overshooting on angle
 
 	def change_speed(self):
 		if self.speed != self.speed_ordered:
@@ -56,7 +66,8 @@ class Ship(object):
 
 	def change_course(self):
 		self.course = self.correct_360_math(self.course + self.speed*self.rudder_angle*self.rate_course_change)
-		if abs(self.course - self.course_ordered) < 1.0:
+		if abs(self.course - self.course_ordered) < 0.5: # course accuracy is with 0.5 degrees
+			# avoids never reaching or constantly oscillating around ordered course
 			self.course = self.course_ordered
 			self.rudder_angle = 0.0
 			self.rudder_angle_ordered = 0.0
